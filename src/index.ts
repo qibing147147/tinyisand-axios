c4e8adcd3af4a2df5b46c5b535838e551a00654e
ustbhuangyi <280309453@qq.com>
2019-03-30 15:50:59 +0800

    feat: fininsh base request
    finish base request and the demos

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
-  if (!config.method) {
-    config.method = 'get'
-  }
   xhr(config)
 }
 
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,7 +1,7 @@
 import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig): void {
-  const { data, url, method } = config
+  const { data = null, url, method = 'get' } = config
 
   const request = new XMLHttpRequest()
 
