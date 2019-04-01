cb79d7f416e322a8502cdf5f45ac0d3fa92341c8
ustbhuangyi <280309453@qq.com>
2019-04-01 17:48:32 +0800

    fix: typo
    typo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
-import { bulidURL } from './helpers/url'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
@@ -13,7 +13,7 @@ function processConfig(config: AxiosRequestConfig): void {
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
-  return bulidURL(url, params)
+  return buildURL(url, params)
 }
 
 export default axios
