70ff4c13a56c2f97cd2a8c52532751b149a13a6c
ustbhuangyi <280309453@qq.com>
2019-04-18 13:29:00 +0800

    feat: header config add options
    header config add options

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -2,6 +2,7 @@ import axios from '../../src/index'
 import qs from 'qs'
 
 axios.defaults.headers.common['test2'] = 123
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8'
 
 axios({
   url: '/config/post',
--- src/defaults.ts
+++ src/defaults.ts
@@ -12,7 +12,7 @@ const defaults: AxiosRequestConfig = {
   }
 }
 
-const methodsNoData = ['delete', 'get', 'head']
+const methodsNoData = ['delete', 'get', 'head', 'options']
 
 methodsNoData.forEach(method => {
   defaults.headers[method] = {}
